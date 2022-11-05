20534d2a80019fd6b72c86e86eec019ae4a6a5ee
BQDuy <106263823+BQDuy@users.noreply.github.com>
2022-11-05 09:59:31 +0700

    TaskMng

---
+++ Task Manager/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.endTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Window;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 426);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process name";
+            this.columnHeader1.Width = 134;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "System memory";
+            this.columnHeader2.Width = 76;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Vitrual memory";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Physical memory";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.endTaskToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(135, 28);
+            // 
+            // endTaskToolStripMenuItem
+            // 
+            this.endTaskToolStripMenuItem.Name = "endTaskToolStripMenuItem";
+            this.endTaskToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.endTaskToolStripMenuItem.Text = "End Task";
+            this.endTaskToolStripMenuItem.Click += new System.EventHandler(this.endTaskToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Task Manager Lite";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem endTaskToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Task Manager/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetProcess();
+        }
+        /// <summary>
+        /// Luu danh sach process
+        /// </summary>
+        Process[] process;
+        /// <summary>
+        /// Lay danh sach process va luu lai
+        /// Dong thoi show len listview
+        /// </summary>
+        void GetProcess()
+        {
+            process = Process.GetProcesses();
+            listView1.Items.Clear();
+            foreach (var item in process)
+            {
+                string name = item.ProcessName.ToString();
+                ListViewItem newItem = new ListViewItem() { Text = chuanHoa(name) };
+                newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = (item.PagedSystemMemorySize64/1024f).ToString("0.0") + " MB" });
+                newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = (item.VirtualMemorySize64/1024f).ToString("0.0") + " MB"});
+                newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = (item.WorkingSet64/1024f).ToString("0.0") + " MB" });
+                listView1.Items.Add(newItem);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(process.Length != Process.GetProcesses().Length)
+            {
+                GetProcess();
+            }
+        }
+
+        public static string chuanHoa(string str1)
+        {
+            return string.Join(" ", str1.Split(' ').Select(name => char.ToUpper(str1[0]) + str1.Substring(1)));
+        }
+
+        private void endTaskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count >0)
+            {
+                int index = 0;
+                foreach( var item in process)
+                {
+                    if(item.ProcessName == listView1.SelectedItems[0].Text)
+                    {
+                        index = process.ToList().IndexOf(item);
+                        break;
+                    }
+                }
+                process[index].Kill();
+            }
+                
+        }
+    }
+}
